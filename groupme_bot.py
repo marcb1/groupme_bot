76af58aabb64f4a2318a8e30028cd3e48865cf03
Marc Bassil <marc.bassil1@gmail.com>
2014-10-10 22:15:35 -0400

    initial commit

---
+++ groupme_bot.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask
+from flask import request
+import requests
+import random
+import codecs
+
+#API id
+#move this to a config file
+bot_id = ''
+
+
+app = Flask(__name__)
+
+#encode string as ASCII
+def stripped(text):
+	text = text.lower()
+	return text.encode('ascii','replace_spc')
+
+def send(text):
+	message = {
+		'text' : text,
+		'bot_id' : bot_id
+	}
+	r = requests.post("https://api.groupme.com/v3/bots/post", params = message)
+
+def handle_message(message):
+        if '' in message.lower():
+          send('Did someone mention me')
+          send("")
+        if '' in message.lower():
+          send('Did someone mention me')
+          send("")
+        
+@app.route('/', methods=['POST'])
+def message():
+	if not request.json or not 'text' in request.json:
+		return
+	
+	user_id = request.json['user_id']
+	nick = request.json['name'].lower()
+	message = request.json['text'].lower()
+	message = stripped(message).strip()
+
+        print 'Got message' + message
+        handle_message(message)
+        return ''
+
+if __name__ == "__main__":
+     app.run(port = 8080, host = '0.0.0.0', debug = True)
